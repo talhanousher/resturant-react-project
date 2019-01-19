d3140f56111d5b47eace631dba1de8fcedc283de
talhanousher <talhanousher@gmail.com>
2019-01-19 23:41:20 +0500

    Added dishe sprmotion and leader files

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -3,16 +3,22 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import Menu from './MenuComponent';
 
 import { DISHES } from '../Shared/dishes';
+import { COMMENTS } from '../Shared/comments';
+import { PROMOTIONS } from '../Shared/promotions';
+import { LEADERS } from '../Shared/leaders';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
             dishes: DISHES,
-            selectedDish: null
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS,
         }
     }
     onDishSelect(dishId) {
@@ -21,7 +27,11 @@ class Main extends Component {
     render() {
         const HomePage = () => {
             return (
-                <Home />
+                <Home
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
             );
         }
         return (
@@ -30,6 +40,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer></Footer>

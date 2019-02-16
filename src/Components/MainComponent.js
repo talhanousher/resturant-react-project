24feb6fdc5948ab6160ce042c96567d6d809842e
talhanousher <talhanousher@gmail.com>
2019-02-16 17:59:01 +0500

    put the form state in redux store

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-
+import { actions } from 'react-redux-form';
 
 import Menu from './MenuComponent';
 import Header from './HeaderComponent';
@@ -50,7 +50,7 @@ class Main extends Component {
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
                     <Route path='/menu/:dishId' component={DishWithId} />
-                    <Route exact path='/contactus' component={Contact} />
+                    <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
                     <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
                     <Redirect to='/home' />
                 </Switch>
@@ -67,7 +67,8 @@ class Main extends Component {
 
 const mapDispatchToProps = dispatch => ({
     addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-    fetchDishes: () => { dispatch(fetchDishes()) }
+    fetchDishes: () => { dispatch(fetchDishes()) },
+    resetFeedbackForm: () => { dispatch(actions.reset('feedback')) }
 });
 
 const mapStateToProps = (state) => {
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,11 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { createForms } from 'react-redux-form';
 
 import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Leaders } from './leaders';
 import { Promotions } from './promotions';
+import { InitialFeedback } from './forms';
 
 export const Store = () => {
     const store = createStore(
@@ -13,7 +15,10 @@ export const Store = () => {
             dishes: Dishes,
             comments: Comments,
             leaders: Leaders,
-            promotions: Promotions
+            promotions: Promotions,
+            ...createForms({
+                feedback: InitialFeedback
+            })
         }),
         applyMiddleware(thunk, logger)
     );
